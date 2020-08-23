2e74ad498f2a784eb0daa4f1601f6cbeb3bbe4ef
Rupesh Pathak <63050454+rupesh2005@users.noreply.github.com>
2020-08-23 20:06:47 +0530

    Create calci_in_python.py

---
+++ calci_in_python.py
@@ -0,0 +1,23 @@
+count = 5
+while count >= 1:
+    print("This is just a Simple Calculator")
+    num_1 = int(input("Please Enter Your First Number: "))
+    num_2 = int(input("Please Enter Your Second Number: "))
+    operator = input("Please Enter The Operator (+ or - or * or /) : ")
+    if operator == '+':
+        print(f'Sum = {num_1 + num_2}')
+        count -= 1
+    elif operator == '-':
+        print(f'Sum = {num_1 - num_2}')
+        count -= 1
+    elif operator == '*':
+        print(f'Sum = {num_1 * num_2}')
+        count -= 1
+    elif operator == '/':
+        print(f'Sum = {num_1 / num_2}')
+        count -= 1
+    else:
+        print(
+            ":( I don't understand that operator, please use '+' for addition, '-' for subtraction, '*' or 'x' for multiplication and '/' for division. "
+        )
+        count -= 1
